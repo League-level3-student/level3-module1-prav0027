a19e192d9a97d53eb563e968a6d4e7512b6501a1
league <league@imac-20.attlocal.net>
2019-11-02 17:27:26 -0700

    GuestBook, Crows, Removing

--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -1,6 +1,15 @@
 package _01_IntroToArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,7 +18,42 @@ public class _02_GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	public static void main(String[] args) {
+		_02_GuestBook book = new _02_GuestBook();
+		book.setUp();
+	}
 	
-	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton();
+	JButton view = new JButton();
+	ArrayList<String> names = new ArrayList<String>();
+
+	public void setUp() {
+		add.setText("Add Name");
+		view.setText("View Names");
+		add.addActionListener(this);
+		view.addActionListener(this);
+		panel.add(add);
+		panel.add(view);
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource().equals(add)) {
+			String enter = JOptionPane.showInputDialog("Please enter a name.");
+			names.add(enter);
+		} else if (e.getSource().equals(view)) {
+			for(int i = 0; i < names.size(); i++){
+				String s = names.get(i);
+				JOptionPane.showMessageDialog(null, "Guest #"+(i+1)+": "+s);
+			}
+		}
+	}
 	
 }
--- src/_01_IntroToArrayLists/_04_RemovingStuffFromArrayLists.java
+++ src/_01_IntroToArrayLists/_04_RemovingStuffFromArrayLists.java
@@ -35,9 +35,12 @@ public class _04_RemovingStuffFromArrayLists {
 		System.out.println(stuffIFoundInTheYard.size());
 
 		/* TODO 1: Clean out the dirt but keep the delicious worms. */
-		
-		
-		
+		for(int i = 0; i<stuffIFoundInTheYard.size(); i++) {
+			if(stuffIFoundInTheYard.get(i).type.equalsIgnoreCase("dirt")) {
+				stuffIFoundInTheYard.remove(i);
+			}
+		}
+		//stuffIFoundInTheYard.removeIf(Dirt)
 		
 		System.out.println(stuffIFoundInTheYard.size()); // should be 2
 
@@ -74,7 +77,14 @@ public class _04_RemovingStuffFromArrayLists {
 		truth.add('r');
 		truth.add('#');
 		/* TODO 2: Remove the hash symbols and print out the truth. */
-
+for(int i = 0; i<truth.size(); i++) {
+	if(truth.get(i) == '#') {
+		truth.remove(i);
+	}
+		}
+for(int i=0; i<truth.size(); i++) {
+	System.out.print(truth.get(i));
+}
 		
 		
 	}
