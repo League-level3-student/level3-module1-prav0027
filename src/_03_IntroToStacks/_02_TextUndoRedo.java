cab009a8c359a1fbc2ea43d12103a2b29cf7567b
league <league@imac-20.attlocal.net>
2019-12-07 17:29:01 -0800

    Text UndoRedo

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -7,56 +7,63 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class _02_TextUndoRedo implements KeyListener{
-	/* 
+public class _02_TextUndoRedo implements KeyListener {
+	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
+	 * Every time a key is pressed, add that character to the JLabel. It should look
+	 * like a basic text editor.
 	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
-	 * Save that deleted character onto a Stack of Characters.
+	 * Make it so that every time the BACKSPACE key is pressed, the last character
+	 * is erased from the JLabel. Save that deleted character onto a Stack of
+	 * Characters.
 	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
-	 * off the Stack and added back to the JLabel.
+	 * Choose a key to be the Undo key. Make it so that when that key is pressed,
+	 * the top Character is popped off the Stack and added back to the JLabel.
 	 * 
-	 * */
-	//static JFrame frame = new JFrame();
+	 */
+	// static JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	String typed = " ";
 	JFrame frame = new JFrame();
+
 	public static void main(String[] args) {
-	setup();
+		_02_TextUndoRedo text = new _02_TextUndoRedo();
+		text.setup();
 	}
 
 	public void setup() {
-	label.setText("You typed: " + typed);
-	panel.add(label);
-	frame.add(panel);
-	frame.setTitle("Type Something");
-	frame.addKeyListener(this);
-	frame.pack();
-	frame.setVisible(true);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-}
+		label.setText("You typed: " + typed);
+		panel.add(label);
+		frame.add(panel);
+		frame.setTitle("Type Something");
+		frame.addKeyListener(this);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("typed something");
-		typed = label.getText()+e.getKeyChar();
+		typed = label.getText() + e.getKeyChar();
 		label.setText(typed);
-		System.out.println(label.getText());
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+if(e.getKeyCode()==8) {
+	int length = typed.length();
+	typed = label.getText().substring(0, length-1);
+	label.setText(typed);
+}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
