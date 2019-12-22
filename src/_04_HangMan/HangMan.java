50c1044807a887ce45e9b95aa5b69b6790b70459
league <league@imac-20.attlocal.net>
2019-12-21 17:25:55 -0800

    Hang Man (IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,35 @@
 package _04_HangMan;
 
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class HangMan{
-	
+	public static void main(String[] args) {
+		String enterednumber = JOptionPane.showInputDialog("Please enter a number.");
+		int number = Integer.parseInt(enterednumber);
+		
+		
+		Stack<String> words = new Stack<String>();
+		for(int i=0; i<number; i++) {
+			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+			//System.out.println(words.pop());
+		}
+		String firstword = words.pop();
+		int charnumber = firstword.length();
+		//System.out.println(charnumber);
+		//System.out.println(firstword);
+		
+		
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel input = new JLabel();
+		JLabel lettersused = new JLabel();
+		
+	}
 	
 	
 }
